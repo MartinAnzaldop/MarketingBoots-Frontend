8d2927584669caed3dbebc453037c0fe977e7d85
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-06-12 23:13:41 -0600

    rama main de frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NadvarComponent } from './components/nadvar/nadvar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NadvarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nadvar/nadvar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nadvar',
+  templateUrl: './nadvar.component.html',
+  styleUrls: ['./nadvar.component.css']
+})
+export class NadvarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
