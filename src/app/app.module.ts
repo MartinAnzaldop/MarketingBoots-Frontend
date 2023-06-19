24b35b71b4c7864842a01f9404526c7fbefebe00
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-06-19 17:38:30 -0600

    creacion de pagina Mb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { NadvarComponent } from './components/nadvar/nadvar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CaracteristicasComponent } from './components/caracteristicas/caracteristicas.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.component';
+import { ListaProductoComponent } from './components/lista-producto/lista-producto.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    NadvarComponent
+    NavbarComponent,
+    CaracteristicasComponent,
+    AcercaComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    FooterComponent,
+    NavbarAdminComponent,
+    ListaProductoComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -20,3 +37,4 @@ import { NadvarComponent } from './components/nadvar/nadvar.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
