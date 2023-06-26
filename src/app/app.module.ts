55838e61ff136b5723195bab7b1fa10714b0cca7
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-06-26 17:26:51 -0600

    se elimino el componnete servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ServiciosComponent } from './components/servicios/servicios.component';
-import { FooterComponent } from './components/footer/footer.component';
+
+
 import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.component';
 import { ListaProductoComponent } from './components/lista-producto/lista-producto.component';
 import { PedidoComponent } from './components/pedido/pedido.component';
@@ -23,8 +23,6 @@ import { ListaPedidosComponent } from './components/lista-pedidos/lista-pedidos.
     AppComponent,
     InicioComponent,
     NavbarComponent,
-    ServiciosComponent,
-    FooterComponent,
     NavbarAdminComponent,
     ListaProductoComponent,
     PedidoComponent,
